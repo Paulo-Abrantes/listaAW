3ddcbe6f896d2c12cd82eee6b8e7ddd0782e94d8
luizneto100 <paivaluiz903@gmail.com>
2025-10-01 11:57:35 -0300

    Q2.js

---
+++ lista/Q2.js
@@ -0,0 +1,12 @@
+document.getElementById("vermelho").onmouseover = function () {
+  document.body.style.backgroundColor = "red";
+};
+document.getElementById("verde").onmouseover = function () {
+  document.body.style.backgroundColor = "green";
+};
+document.getElementById("azul").onmouseover = function () {
+  document.body.style.backgroundColor = "blue";
+};
+document.getElementById("amarelo").onmouseover = function () {
+  document.body.style.backgroundColor = "yellow";
+};
