1dc6eaae52fbc61d58c7d70671a0a46dc827707d
luizneto100 <paivaluiz903@gmail.com>
2025-10-01 11:58:28 -0300

    Q4.js

---
+++ lista/Q4.js
@@ -0,0 +1,15 @@
+function ehPalindromo(palavra) {
+  const invertida = palavra.split("").reverse().join("");
+  return palavra === invertida;
+}
+
+document.getElementById("verificarBtn").onclick = function() {
+  const palavra = document.getElementById("palavra").value;
+  const resultado = document.getElementById("resultado");
+
+  if (ehPalindromo(palavra)) {
+    resultado.textContent = `"${palavra}" é um palíndromo ✅`;
+  } else {
+    resultado.textContent = `"${palavra}" não é um palíndromo ❌`;
+  }
+};
